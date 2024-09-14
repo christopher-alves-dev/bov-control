6a259bb1d9a312e94b56fc232d270ae67c20c184
Christopher Alves <christopher.alves.dev@gmail.com>
2024-09-14 19:07:11 -0300

    feat: create card component

---
+++ src/components/card/index.tsx
@@ -0,0 +1,42 @@
+import Entypo from "@expo/vector-icons/Entypo";
+import FontAwesomeIcons from "@expo/vector-icons/FontAwesome5";
+import { CheckList } from "@services/api/types";
+import React, { ComponentProps } from "react";
+import { TouchableOpacity } from "react-native";
+
+import { formatDate } from "@utils/format-date";
+import * as S from "./styles";
+
+type Props = {
+  data: CheckList;
+  onPress: ComponentProps<typeof TouchableOpacity>["onPress"];
+};
+
+export const Card = ({ data, onPress }: Props) => {
+  const creationData = formatDate(data.created_at);
+
+  return (
+    <S.Container onPress={onPress}>
+      <S.Header>
+        <S.Title>{data.farmer.name}</S.Title>
+      </S.Header>
+
+      <S.Body>
+        <S.Row>
+          <FontAwesomeIcons name="user-alt" size={20} />
+          <S.InfoText>{data.from.name}</S.InfoText>
+        </S.Row>
+
+        <S.Row>
+          <Entypo name="location-pin" size={20} />
+          <S.InfoText>{data.farmer.city}</S.InfoText>
+        </S.Row>
+
+        <S.Row>
+          <FontAwesomeIcons name="calendar-day" size={20} />
+          <S.InfoText>{creationData}</S.InfoText>
+        </S.Row>
+      </S.Body>
+    </S.Container>
+  );
+};
---
+++ src/components/card/styles.ts
@@ -0,0 +1,36 @@
+import styled from "styled-components/native";
+
+export const Container = styled.TouchableOpacity`
+  gap: 16px;
+  border-radius: 8px;
+  background-color: ${({ theme }) => theme.colors.white};
+  overflow: hidden;
+`;
+
+export const Header = styled.View`
+  padding: 12px;
+  background-color: ${({ theme }) => theme.colors.blue[600]};
+`;
+
+export const Title = styled.Text`
+  color: ${({ theme }) => theme.colors.white};
+  font-size: 20px;
+  font-family: ${({ theme }) => theme.fonts.bold};
+`;
+
+export const Body = styled.View`
+  padding: 0 16px 16px;
+  gap: 8px;
+`;
+
+export const Row = styled.View`
+  flex-direction: row;
+  align-items: center;
+  gap: 8px;
+`;
+
+export const InfoText = styled.Text`
+  color: ${({ theme }) => theme.colors.black};
+  font-size: 14px;
+  font-family: ${({ theme }) => theme.fonts.regular};
+`;
