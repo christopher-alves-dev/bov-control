d22a7f560b33a8fee0d8c7a73d036c92b50f70c1
Christopher Alves <christopher.alves.dev@gmail.com>
2024-09-14 22:10:36 -0300

    feat: create show checklist service

--- src/services/api/checklist/index.ts
+++ src/services/api/checklist/index.ts
@@ -1,8 +1,12 @@
 import { api } from "@services/api";
-import { IndexCheckListResponse } from "../types";
+import { IndexCheckListResponse, ShowCheckListResponse } from "./types";
 
 const index = () => api.get<IndexCheckListResponse>("/v1/checkList");
 
+const show = (id: string) =>
+  api.get<ShowCheckListResponse>(`/v1/checkList/${id}`);
+
 export const checkListApi = {
   index,
+  show,
 };
